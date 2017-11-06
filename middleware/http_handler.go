99bfaf1d4fc8e5a6339de53dd49d8b9691985bc0
Bas van Beek <bas@xacte.com>
2017-11-06 16:47:29 +1100

    added a HTTP Handler Middleware (WrapHTTPHandler) for Auto instrumentation of HTTP Server side requests

---
+++ middleware/http_handler.go
@@ -0,0 +1,88 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+
+	zipkin "github.com/openzipkin/zipkin-go"
+	"github.com/openzipkin/zipkin-go/kind"
+	"github.com/openzipkin/zipkin-go/propagation/b3"
+	"github.com/openzipkin/zipkin-go/propagation/context/span"
+)
+
+type httpHandler struct {
+	tracer *zipkin.Tracer
+	name   string
+	next   http.Handler
+}
+
+// WrapHTTPHandler wraps a standard http.Handler with Zipkin tracing.
+func WrapHTTPHandler(t *zipkin.Tracer, op string, h http.Handler) http.Handler {
+	return &httpHandler{tracer: t, next: h, name: op}
+}
+
+// ServeHTTP implements http.Handler.
+func (h httpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// try to extract B3 Headers from upstream
+	sc := h.tracer.Extract(b3.ExtractHTTP(r))
+
+	// create Span using SpanContext if found
+	sp := h.tracer.StartSpan(h.name, kind.Server, zipkin.Parent(sc))
+	defer sp.Finish()
+
+	// add our span to context
+	ctx := span.NewContext(r.Context(), sp)
+
+	// tag typical HTTP request items
+	zipkin.TagHTTPMethod.Set(sp, r.Method)
+	zipkin.TagHTTPUrl.Set(sp, r.URL.String())
+	zipkin.TagHTTPRequestSize.Set(sp, fmt.Sprintf("%d", r.ContentLength))
+
+	// set remote HTTP endpoint based on RemoteAddr
+	sp.SetRemoteEndpoint(zipkin.NewEndpointOrNil("", r.RemoteAddr))
+
+	// create http.ResponseWriter interceptor for tracking response size and
+	// status code.
+	ri := &rwInterceptor{w: w, statusCode: 200}
+
+	// tag found response size and status code on exit
+	defer func() {
+		zipkin.TagHTTPStatusCode.Set(sp, ri.getStatusCode())
+		zipkin.TagHTTPResponseSize.Set(sp, ri.getResponseSize())
+	}()
+
+	// call next http Handler func using our updated context.
+	h.next.ServeHTTP(ri, r.WithContext(ctx))
+}
+
+// rwInterceptor intercepts the ResponseWriter so it can track response size
+// and returned status code.
+type rwInterceptor struct {
+	w          http.ResponseWriter
+	size       uint64
+	statusCode int
+}
+
+func (r *rwInterceptor) Header() http.Header {
+	return r.w.Header()
+}
+
+func (r *rwInterceptor) Write(b []byte) (n int, err error) {
+	n, err = r.w.Write(b)
+	atomic.AddUint64(&r.size, uint64(n))
+	return
+}
+
+func (r *rwInterceptor) WriteHeader(i int) {
+	r.statusCode = i
+	r.w.WriteHeader(i)
+}
+
+func (r *rwInterceptor) getStatusCode() string {
+	return fmt.Sprintf("%d", r.statusCode)
+}
+
+func (r *rwInterceptor) getResponseSize() string {
+	return fmt.Sprintf("%d", atomic.LoadUint64(&r.size))
+}
