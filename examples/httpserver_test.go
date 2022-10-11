8f980e245a8e5f6c898b2d18d52cdb71c4eebef6
Bas van Beek <bas@tobin.nl>
2022-10-11 20:12:29 +0200

    remove external dependency in example

--- examples/httpserver_test.go
+++ examples/httpserver_test.go
@@ -21,9 +21,7 @@ import (
 	"os"
 	"time"
 
-	"github.com/gorilla/mux"
-
-	zipkin "github.com/openzipkin/zipkin-go"
+	"github.com/openzipkin/zipkin-go"
 	zipkinhttp "github.com/openzipkin/zipkin-go/middleware/http"
 	logreporter "github.com/openzipkin/zipkin-go/reporter/log"
 )
@@ -31,7 +29,9 @@ import (
 func Example() {
 	// set up a span reporter
 	reporter := logreporter.NewReporter(log.New(os.Stderr, "", log.LstdFlags))
-	defer reporter.Close()
+	defer func() {
+		_ = reporter.Close()
+	}()
 
 	// create our local service endpoint
 	endpoint, err := zipkin.NewEndpoint("myService", "localhost:0")
@@ -57,27 +57,29 @@ func Example() {
 	}
 
 	// initialize router
-	router := mux.NewRouter()
+	router := http.NewServeMux()
 
 	// start web service with zipkin http server middleware
 	ts := httptest.NewServer(serverMiddleware(router))
 	defer ts.Close()
 
 	// set-up handlers
-	router.Methods("GET").Path("/some_function").HandlerFunc(someFunc(client, ts.URL))
-	router.Methods("POST").Path("/other_function").HandlerFunc(otherFunc(client))
+	router.HandleFunc("/some_function", someFunc(client, ts.URL))
+	router.HandleFunc("/other_function", otherFunc(client))
 
 	// initiate a call to some_func
-	req, err := http.NewRequest("GET", ts.URL+"/some_function", nil)
+	var req *http.Request
+	req, err = http.NewRequest("GET", ts.URL+"/some_function", nil)
 	if err != nil {
 		log.Fatalf("unable to create http request: %+v\n", err)
 	}
 
-	res, err := client.DoWithAppSpan(req, "some_function")
+	var res *http.Response
+	res, err = client.DoWithAppSpan(req, "some_function")
 	if err != nil {
 		log.Fatalf("unable to do http request: %+v\n", err)
 	}
-	res.Body.Close()
+	_ = res.Body.Close()
 
 	// Output:
 }
@@ -105,17 +107,18 @@ func someFunc(client *zipkinhttp.Client, url string) http.HandlerFunc {
 
 		newRequest = newRequest.WithContext(ctx)
 
-		res, err := client.DoWithAppSpan(newRequest, "other_function")
+		var res *http.Response
+		res, err = client.DoWithAppSpan(newRequest, "other_function")
 		if err != nil {
 			log.Printf("call to other_function returned error: %+v\n", err)
 			http.Error(w, err.Error(), 500)
 			return
 		}
-		res.Body.Close()
+		_ = res.Body.Close()
 	}
 }
 
-func otherFunc(client *zipkinhttp.Client) http.HandlerFunc {
+func otherFunc(_ *zipkinhttp.Client) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("other_function called with method: %s\n", r.Method)
 		time.Sleep(50 * time.Millisecond)
